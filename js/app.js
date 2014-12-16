ca18e9a686abfb409aa651b407d3af3883b69bd7
szewei <szewei@sam-media.com>
2014-12-16 22:57:53 +0800

    continue planning

--- js/app.js
+++ js/app.js
@@ -4,31 +4,11 @@ var app =  angular.module('demoApp', []);
 app.controller('demoController', function ($scope) {
     
     $scope.datas = datas;
-    
-        
-    $scope.results = _.findWhere($scope.datas, {gender: 'female'});
-    
-    //$scope.results = _.where($scope.datas, {gender: 'female'});
-    
-    
-//    $scope.results = _.find($scope.datas, function(data){
-//        return data.age > 50
-//    })
-    
-//    $scope.results = _.filter($scope.datas, function(data){
-//        return data.age < 30 && data.gender == 'female' 
-//    });
-    
-//    $scope.results = _.reject($scope.datas, function(data){
-//        return data.age > 50 
-//    });  
-    
-//    $scope.results = _.every($scope.datas, function(data){
-//        return data.friends 
-//    }); 
-    
-//      $scope.results = _.str.capitalize('epeli');
 
-    
-    
+    $scope.results = _.chain($scope.datas)
+        .pluck('friends')
+        .flatten(true)
+        .pluck('name')
+        .value();
+
 });
--- js/command.js
+++ js/command.js
@@ -19,19 +19,43 @@ app.controller('allController', function ($scope) {
         return data.age > 50
     });
 
+    $scope.results = _.max($scope.datas, function(data){
+        return data.age;
+    });
+
 
     //checking some status for collection
-
     $scope.results = _.every($scope.datas, function(data){
         return data.friends
     });
 
     $scope.results = _.some($scope.datas, function(data){
-        return data.phone.match(//)
+        return data.phone.match('(850)')
     });
+
+    //modify the list
+    $scope.results = _.reduce($scope.datas, function(memo, data){
+        return memo + data.age
+    }, 0);
+
+    $scope.results = _.pluck($scope.datas, 'name');
+
+    $scope.results = _.map($scope.datas, function(data){
+        return (data.gender == 'male' ? 'Mr. ' : 'Ms. ') + data.name;
+    });
+
+    $scope.results = _.chain($scope.datas)
+        .pluck('friends')
+        .flatten(true)
+        .pluck('name')
+        .value();
 
 
     //converting string
+    $scope.results = _.str.capitalize('epeli');
+
+    $scope.results = _.str.capitalize('epeli');
+
     $scope.results = _.str.capitalize('epeli');
 
 
